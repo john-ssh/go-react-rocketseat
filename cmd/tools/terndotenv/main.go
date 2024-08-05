3fed9ad176ba6d8924d61c4c90a3ee95984a7f51
john-ssh <th3gentl3lm3nt@gmail.com>
2024-08-05 20:43:24 -0300

    Set up go enviroment and docker also the database

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "os/exec"
+import "github.com/joho/godotenv"
+
+func main() {
+  if err := godotenv.Load(); err != nil {
+    panic(err)
+  }
+
+  cmd := exec.Command(
+    "tern",
+    "./home/john/go/bin/tern",
+    "migrate",
+    "--migrations",
+    "./internal/store/pgstore/migrations",
+    "--config",
+    "./internal/store/pgstore/migrations/tern.conf",
+    )
+  if err := cmd.Run(); err != nil {
+    panic(err)
+  }
+}
